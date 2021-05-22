dfb9d3ea2ea9edfdbcfc096f0aedf34edb6cba28
kirestein <erik.proenca2011@gmail.com>
2021-05-22 19:58:35 -0300

    entendo o for

---
+++ estruturaRepeticao.js
@@ -0,0 +1,16 @@
+
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+var carros = ["Palio", "Tiida", "Sandero", "Santana", "Gol"];
+
+for (var i = 0; i < carros.length; i++) {
+    console.log(carros[i]);
+    
+}
+
+for (var i = carros.length - 1; i >=0; i--) {
+    console.log(carros[i]);
+}

a25f99d2c80863a5bf4f70a18706a8dfe3007dd7
kirestein <erik.proenca2011@gmail.com>
2021-05-22 11:36:04 -0300

    trabalhando com operadores

---
+++ operadores.js
@@ -0,0 +1,11 @@
+var soma = 4 + 2;
+var subtracao = 4 - 2;
+var multiplicacao = 4 * 2;
+var divisao = 4 / 2;
+var resto = 4 % 3;
+
+console.log("a soma 4 + 2 é " + soma);
+console.log("a subtração 4 - 2 é " + subtracao);
+console.log("a multiplicação 4 x 2 é " + multiplicacao);
+console.log("a divisão 4 por 2 é " + divisao);
+console.log("o resto da divisão de 4 por 3 é " + resto);

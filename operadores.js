74bc2b9cc627104a7836a104fdb5e9db0b5776c9
kirestein <erik.proenca2011@gmail.com>
2021-05-22 12:02:23 -0300

    Trabalhando com operadores, incrementos e decrementos

--- operadores.js
+++ operadores.js
@@ -4,8 +4,53 @@ var multiplicacao = 4 * 2;
 var divisao = 4 / 2;
 var resto = 4 % 3;
 
-console.log("a soma 4 + 2 é " + soma);
-console.log("a subtração 4 - 2 é " + subtracao);
-console.log("a multiplicação 4 x 2 é " + multiplicacao);
-console.log("a divisão 4 por 2 é " + divisao);
-console.log("o resto da divisão de 4 por 3 é " + resto);
+// console.log("a soma 4 + 2 é " + soma);
+// console.log("a subtração 4 - 2 é " + subtracao);
+// console.log("a multiplicação 4 x 2 é " + multiplicacao);
+// console.log("a divisão 4 por 2 é " + divisao);
+// console.log("o resto da divisão de 4 por 3 é " + resto);
+
+// var incremento = 1;
+
+// incremento = incremento + 1;
+// incremento++;
+// console.log("O incremento é " + incremento);
+
+// var atribuicaoComAdicao = 5;
+
+// atribuicaoComAdicao += 6;
+
+// console.log("O valor de atribuição com adição é " + atribuicaoComAdicao);
+
+// var decremento = 5;
+
+// decremento--;
+
+// console.log("o decremento de 5 é " + decremento);
+
+// var decrementoComSubtracao = 10;
+
+// decrementoComSubtracao -= 4;
+
+// console.log("o decremento de 10 com subtração de quatro é " + decrementoComSubtracao);
+
+// var atribuicaoComMultiplicacao = 4;
+
+// atribuicaoComMultiplicacao *= 5;
+
+// console.log("a atribuição de 4 com multiplicação por 5 é " + atribuicaoComMultiplicacao);
+
+// var atribuicaoComDivisao = 20;
+
+// atribuicaoComDivisao /= 4;
+
+// console.log("a atribuição de 20 com divisão por 4 é " + atribuicaoComDivisao);
+
+var a = 4;
+var b = 3;
+
+var adicaoIncrementoPosterior = a + b++;
+console.log("a adição com incremento posterior é " + adicaoIncrementoPosterior);
+
+var adicaoIncrementoAnterior = a + ++b;
+console.log("a adição com incremento anterior é " + adicaoIncrementoAnterior);

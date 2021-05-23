7d45a744381709ce7e45b04f34a123f37251fc76
kirestein <erik.proenca2011@gmail.com>
2021-05-23 13:32:32 -0300

    Resolvendo o desafio do primeiro módulo

---
+++ desafioModulo1.js
@@ -0,0 +1,25 @@
+var funcionario = [];
+
+funcionario[0] = {
+    nome: "Alfredo",
+    salario: "R$" + 1500 + ",00",
+    setor: "Produção"
+}
+
+funcionario[1] = {
+    nome: "Roberto",
+    salario: "R$" + 2600 + ",00",
+    setor: "Vendas"
+}
+
+funcionario[2] = {
+    nome: "Shiba",
+    salario: "R$" + 3500 + ",00",
+    setor: "Programação"
+}
+
+function cadastro(funcionario) {
+    return funcionario;
+}
+
+cadastro(funcionario);

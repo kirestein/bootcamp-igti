3a8e53e45e92c1a78ad647b48599952d9836faa8
kirestein <erik.proenca2011@gmail.com>
2021-05-23 08:59:06 -0300

    Modificação de ;

--- calculadora.js
+++ calculadora.js
@@ -1,33 +1,9 @@
-var operacao = "potencia";
+console.log(process.argv)
+
+var operacao = "";
 var num1 = 4;
 var num2 = 3;
 var resultado = 0;
-
-switch (operacao) {
-    case "soma":
-        resultado = num1 + num2;
-        break;
-    case "subracao":
-        resultado = num1 - num2;
-        break;
-    case "multiplicacao":
-        resultado = num1 * num2;
-        break;
-    case "divisao":
-        resultado = num1 / num2;
-        break;
-    case "resto":
-        resultado = num1 % num2;
-        break;
-    case "porcentagem":
-        resultado = num1 * num2 / 100;
-        break;
-    case "potencia":
-        resultado = num1 ** num2;
-        break;
-    default:
-        resultado = 0;
-}
 
 if (operacao === "soma") {
     resultado = num1 + num2;

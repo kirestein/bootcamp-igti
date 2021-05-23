72c80038b3a15a7594148bd50fe09fde86d47176
kirestein <erik.proenca2011@gmail.com>
2021-05-23 16:43:58 -0300

    Criando um jogo de adivinhação

---
+++ adivinhacao.js
@@ -0,0 +1,7 @@
+/*
+    Criar um jogo no qual o jogador tenha que adivinhar um número. O jogo deve gerar
+    um número aleatório entre 1 e 100, e depois desafiar o jogador a descobrir qual o número
+    em no máximo 10 tentativas. A cada tentativa, caso o jogador não tenha acertado o número,
+    o jogo deve unformar se o número informado é maior ou menor que o sorteado.
+    O jogo termina se o jogador acertar o número ou acabarem o número de tentativas.
+*/

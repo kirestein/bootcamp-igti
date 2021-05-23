c8ef4c15ddec265cfa83eacf61e03d6a88f6301e
kirestein <erik.proenca2011@gmail.com>
2021-05-23 19:56:41 -0300

    Criando uma estratégia para criar o jogo

--- adivinhacao.js
+++ adivinhacao.js
@@ -4,4 +4,20 @@
     em no máximo 10 tentativas. A cada tentativa, caso o jogador não tenha acertado o número,
     o jogo deve unformar se o número informado é maior ou menor que o sorteado.
     O jogo termina se o jogador acertar o número ou acabarem o número de tentativas.
+
+    1. Gerar um número aleatório entre 1 e 100 (biblioteca math - função random);
+    2. Inicializar o número de tentativas como 10;
+    3. Pedri ao jogador para tentar adivinhar o número;
+    4. Decrementar o número de tentativas;
+    5. Verificar se a tentativa está correta;
+    6. Se a tentativa estiver correta:
+        - Informar que acertou o número;
+        - Encerrar o jogo;
+    7. Se a tentativa estiver incorreta, e acabaram as tentativas;
+        - informar que ele não venceu.
+        - Encerrar o jogo;
+    8. Se a tentativa estiver incorreta e ainda tem tentativa;
+        - Informar se a tentativa é maior ou menor que o número sorteado;
+        - Pedir outra tentativa para o jogador.
 */
+

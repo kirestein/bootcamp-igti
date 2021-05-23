2d74f89fa1c06cb2b181b19a5c3b8d4a7e34d35c
kirestein <erik.proenca2011@gmail.com>
2021-05-23 16:22:58 -0300

    Finalizando o arquivo

--- arquivos.js
+++ arquivos.js
@@ -1,8 +1,26 @@
 var fs = require("fs");
-fs.writeFile("meuArquivo.txt", "Um texto qualquer", function(err) {
+var nomeArquivo = "meuArquivo.txt"
+
+fs.writeFile(nomeArquivo, "Um texto qualquer", function(err) {
     if (err !== null) {
         console.log(err);
     }else{
         console.log("arquivo escrito com sucesso");
+            // para adicionar no arquivo
+        fs.appendFile(nomeArquivo, "\nnova linha no texto", function(err) {
+            if (err) {
+                console.log(err);
+            }else{
+                fs.readFile(nomeArquivo, "utf-8", function(err, data) {
+                if (err) {
+                console.log(err);    
+                }else{
+                    console.log(data);
+                }
+                });
+            }
+        });
+
+        
     }
 });

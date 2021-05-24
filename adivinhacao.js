5d08b64f61a31644610ad9aeb2c8ee69a4b34ac1
kirestein <erik.proenca2011@gmail.com>
2021-05-23 22:02:25 -0300

    Finalização do jogo de adivinhação

--- adivinhacao.js
+++ adivinhacao.js
@@ -7,7 +7,7 @@
 
     1. Gerar um número aleatório entre 1 e 100 (biblioteca math - função random);
     2. Inicializar o número de tentativas como 10;
-    3. Pedri ao jogador para tentar adivinhar o número;
+    3. Pedir ao jogador para tentar adivinhar o número;
     4. Decrementar o número de tentativas;
     5. Verificar se a tentativa está correta;
     6. Se a tentativa estiver correta:
@@ -20,4 +20,40 @@
         - Informar se a tentativa é maior ou menor que o número sorteado;
         - Pedir outra tentativa para o jogador.
 */
+var readline = require("readline");
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var numeroAleatorio = Math.round(Math.random() * 100);
+if (numeroAleatorio === 0) {
+    numeroAleatorio = 1;
+}
+
+
+var numeroTentativa = 10;
+
+pergunta();
+
+function pergunta() {
+rl.question("Digite um número de 1 a 100(você tem " + numeroTentativa + " chances): ", function(numero) {
+    console.log(numero);
+    numero = parseInt(numero);
+    numeroTentativa--;
+
+    if (numero === numeroAleatorio) {
+        console.log("Parabéns, você venceu o jogo");
+        rl.close();
+    } else if (numeroTentativa === 0){
+        console.log("Você não conseguiu a certar com as 10 tentativas");
+        rl.close();
+    } else if (numero < numeroAleatorio) {
+        console.log("O número " + numero + " é menor que o número sorteado, você ainda tem " + numeroTentativa + " chances");
+        pergunta();
+    } else {
+        console.log("O número " + numero + " é maior que o número sorteado, você ainda tem " + numeroTentativa + " chances")
+        pergunta();
+    };
+    })}
 

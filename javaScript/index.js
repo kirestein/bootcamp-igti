b39b0285b094dcd740b487cbcd077c05b20af8dc
kirestein <erik.proenca2011@gmail.com>
2021-05-26 16:16:12 -0300

    Manipulando HTML através do JS

--- javaScript/index.js
+++ javaScript/index.js
@@ -9,3 +9,13 @@ setTimeout(function() {
     console.log("passou 2 segundo")
     // document.write("texto qualquer");
 }, 2000);
+
+function clickBotao() {
+    // document.getElementById("paragrafo2").innerHTML = "Texto alterado com sucesso";
+    document.getElementById("paragrafo2").style.display =  "none";
+
+};
+
+function clickBotao2() {
+    document.getElementById("paragrafo2").style.display = "block"
+}

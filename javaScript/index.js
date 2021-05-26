28ac89f324cac2ea3687aeed87e7e2a8d4d6de50
kirestein <erik.proenca2011@gmail.com>
2021-05-26 20:02:17 -0300

    Manipulação do evento ligar / desligar lâmpada através do js

--- javaScript/index.js
+++ javaScript/index.js
@@ -18,4 +18,12 @@ function clickBotao() {
 
 function clickBotao2() {
     document.getElementById("paragrafo2").style.display = "block"
-}
+};
+
+function ligarLampada( ) {
+    document.getElementById("lampada").src = "/img/lampada-ligada.jpg";
+};
+
+function desligarLampada( ) {
+    document.getElementById("lampada").src = "/img/lampada-desligada.jpg";
+};

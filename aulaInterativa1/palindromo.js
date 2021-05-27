ffd07ffd48af52d747cfcb1a076e8439e32c5840
kirestein <erik.proenca2011@gmail.com>
2021-05-26 22:44:58 -0300

    Criação de um programa para determinar se uma palavra é ou não um palindromo

--- aulaInterativa1/palindromo.js
+++ aulaInterativa1/palindromo.js
@@ -1,8 +1,19 @@
 
 // criação da variável para armazenar a palavra
-var palavra = "arara";
+var palavra = "erik";
 var palavraInvertida = "";
 
-for (var i = 0; i < 5; i++) {
+/*for (var i = 0; i < palavra.length; i++) {
+    palavraInvertida += palavra[i];
+}*/
 
-}
+for (var i = palavra.length-1; i >= 0; i--) {
+    palavraInvertida = palavraInvertida + palavra[i];
+    }
+    if (palavraInvertida === palavra) {
+        console.log("A palavra " + palavra + " é um palindromo.");
+    }else if (palavraInvertida !== palavra) {
+        console.log("A palavra " + palavra + " não é um palindromo.");
+};
+
+// console.log(palavraInvertida);

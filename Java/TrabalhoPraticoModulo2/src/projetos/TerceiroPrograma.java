a14422c1697404222126b4561fa0af8850de9f77
= <=>
2021-06-05 15:10:24 -0300

    Criação do terceiro programa do trabalho

---
+++ Java/TrabalhoPraticoModulo2/src/projetos/TerceiroPrograma.java
@@ -0,0 +1,31 @@
+package projetos;
+import java.util.Scanner;
+public class TerceiroPrograma {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		double nota1 = 0, nota2 = 0, nota3 = 0, notaTotal = 0, frequencia = 0;
+		double notaCorte = 50, freqCorte = 60;
+		
+		System.out.println("Digute a primeiro nota..: ");
+		nota1 = entrada.nextDouble();
+		System.out.println("Digite a segunda nota..: ");
+		nota2 = entrada.nextDouble();
+		System.out.println("Digite a terceira nota..: ");
+		nota3 = entrada.nextDouble();
+		System.out.println("Digite a frequência..: ");
+		frequencia = entrada.nextDouble();
+		
+		notaTotal = nota1 + nota2 + nota3;
+		if (notaTotal >= notaCorte && frequencia >= freqCorte)
+			System.out.println("Aprovado.");
+		else if (notaTotal>= (notaCorte - notaCorte * 0.20) && frequencia >= freqCorte)
+			System.out.println("Prova Especial.");
+		else if (notaTotal >= 80)
+			System.out.println("Aprovado direto.");
+		else
+			System.out.println("Reprovado.");
+
+	}
+
+}

83157c7b3bd035ac27f0b94872b2f6a107e707ff
= <=>
2021-06-05 15:26:42 -0300

    Testando os valores da prova no programa 2

--- Java/TrabalhoPraticoModulo2/src/projetos/SegundoPrograma.java
+++ Java/TrabalhoPraticoModulo2/src/projetos/SegundoPrograma.java
@@ -1,7 +1,6 @@
 package projetos;
 import java.util.Scanner;
 public class SegundoPrograma {
-
 	public static void main(String[] args) {
 		int valor1 = 0, valor2 = 75, valor3 = 0;
 		Scanner ent = new Scanner(System.in);
@@ -10,13 +9,16 @@ public class SegundoPrograma {
 		valor1 = ent.nextInt();
 		valor2 += valor1;
 		
+		//System.out.printf("O valor da variável 2 é: %d \n", valor2);
+		
 		if (valor1 % 2 == 0)
 			valor3 = valor1 * 5;
 		else if (valor1 % 3 == 0)
 			valor3 = valor2 * 9;
 		else
 			valor3 = valor2;
-		System.out.printf("O valor da variável valor3 é: %d.", valor3);
+		System.out.printf("O valor da variável valor3 é: %d.\n", valor3);
+		System.out.printf("O valor da variável 2 é: %d \n", valor2);
 
 	}
 

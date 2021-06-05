2618317baceb87fcd341bfa16787b2a1fce52189
kirestein <erik.proenca2011@gmail.com>
2021-06-04 21:18:35 -0300

    Trabalhando com tipos primitivos

---
+++ Java/ProjetoAula2_4/src/pacoteAula2_4/ClasseAula2_4.java
@@ -0,0 +1,23 @@
+package pacoteAula2_4;
+
+public class ClasseAula2_4 {
+
+	public static void main(String[] args) {
+		
+		int nota = 20;
+		char tipoAluno = 'B';
+		double horasCursadas = 2.5;
+		boolean aprovado = true;
+		
+		System.out.println("Nota: " + nota);
+		System.out.println("Tipo de Aluno: " + tipoAluno);
+		System.out.println("Horas Cursadas: " + horasCursadas);
+		System.out.println("Aprovado: " + aprovado);
+		
+		nota = 40; // Só precisamos colocar o tipo da variável quando estamos declarando a variável.
+		
+		System.out.println("Nota " + nota);
+
+	}
+
+}

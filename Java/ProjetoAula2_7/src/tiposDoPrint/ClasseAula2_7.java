11d05c63eed410131329b38b7393c30cfb34d0d7
kirestein <erik.proenca2011@gmail.com>
2021-06-04 21:58:53 -0300

    Estudando print, println e printf

--- Java/ProjetoAula2_7/src/tiposDoPrint/ClasseAula2_7.java
+++ Java/ProjetoAula2_7/src/tiposDoPrint/ClasseAula2_7.java
@@ -4,8 +4,21 @@ public class ClasseAula2_7 {
 
 	public static void main(String[] args) {
 		int idade = 36;
+		double altura = 1.85;
+		String nome = "Erik Proença";
+		boolean casado = true;
 		
-		System.out.print("Idade = " + idade);
+		// %s é coringa do String
+		// %d é coringa do inteiro
+		//%.2f float ou double com duas casa decimais
+		//%b é o coringa do boolean
+		//%c é o coringa do char
+		
+		System.out.printf("O %s tem idade igual a %d e a altura igual a %.2f e o "
+				+ "fato de ele ser casado é %b", nome, idade, altura, casado);
+		
+		//System.out.println("Idade = " + idade);
+		//System.out.print("Altura = " + altura);
 
 	}
 

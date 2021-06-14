349dc391799c85d46e2019e9e072a87480b079c6
= <=>
2021-06-13 23:40:18 -0300

    Imprimindo as tabuadas com o for

--- Java/ProjetoAula3_8/src/for_while_dowhile/Aula_for.java
+++ Java/ProjetoAula3_8/src/for_while_dowhile/Aula_for.java
@@ -6,10 +6,13 @@ public class Aula_for {
 		
 		//for (inicialização; condição; incremento)
 		
-		var i = 0;
+		int i = 0, j = 0;
 		
-		for (i = 0; i < 10; i++) {
-			System.out.printf("O valor de i é: %d \n", i);
+		for (i = 0; i <= 10; i++) {
+			System.out.printf("Tabuada do %d \n", i);
+			for (j = 0; j <= 10; j++) {
+				System.out.printf("%d x %d = %d. \n", i, j, i*j);
+			}
 		}
 
 	}

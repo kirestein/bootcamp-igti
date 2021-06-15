ec3a2ef7621d96d7836288d577e8af389ff8b2d9
= <=>
2021-06-15 08:36:57 -0300

    Ajustando a tabuada usando o for

--- Java/ProjetoAula3_8/src/for_while_dowhile/Aula_for.java
+++ Java/ProjetoAula3_8/src/for_while_dowhile/Aula_for.java
@@ -8,9 +8,9 @@ public class Aula_for {
 		
 		int i = 0, j = 0;
 		
-		for (i = 0; i <= 10; i++) {
+		for (i = 1; i <= 10; i++) {
 			System.out.printf("Tabuada do %d \n", i);
-			for (j = 0; j <= 10; j++) {
+			for (j = 1; j <= 10; j++) {
 				System.out.printf("%d x %d = %d. \n", i, j, i*j);
 			}
 		}

93b20189affc11a95d2345c3e1368a14b4f95ad5
= <=>
2021-06-15 08:42:41 -0300

    Praticando o while

--- Java/ProjetoAula3_8/src/for_while_dowhile/Aula_while.java
+++ Java/ProjetoAula3_8/src/for_while_dowhile/Aula_while.java
@@ -1,9 +1,18 @@
 package for_while_dowhile;
 
+import java.util.Scanner;
+
 public class Aula_while {
 
 	public static void main(String[] args) {
+		char saida = 'N';
 		
+		Scanner entrada = new Scanner(System.in);
+		
+		while (saida != 'S') {
+			System.out.println("Gostaria de continuar? (S - Sim / N - Não ");
+			saida = entrada.nextLine().charAt(0);
+		}
 
 	}
 

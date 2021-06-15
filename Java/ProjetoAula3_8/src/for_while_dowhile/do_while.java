6516ff8a75e9442a870c6ab5968e567570acd87d
= <=>
2021-06-15 09:04:43 -0300

    Tabalhando com do while

---
+++ Java/ProjetoAula3_8/src/for_while_dowhile/do_while.java
@@ -0,0 +1,29 @@
+package for_while_dowhile;
+
+import java.util.Scanner;
+
+public class do_while {
+	
+	public static void main(String[] args) {
+		
+		char continuar = ' ';
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Deseja continuar? (s = sim / n = não)");
+		continuar = entrada.nextLine().charAt(0);
+		
+		while (continuar == 's') {
+			System.out.println("Mensagem");
+			System.out.println("Deseja continuar? (s = sim / n = não)");
+			continuar = entrada.nextLine().charAt(0);
+		}
+		do {
+			System.out.println("Mensagem");
+		} while (continuar == 's');
+		
+		System.out.println("Fim do programa!");
+		
+	}
+	
+
+}

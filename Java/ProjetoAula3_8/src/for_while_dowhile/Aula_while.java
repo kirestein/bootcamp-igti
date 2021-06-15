125e7c4245f1bb28ac836d03efd3f91f580d9606
= <=>
2021-06-15 08:54:23 -0300

    Aprendendo While

--- Java/ProjetoAula3_8/src/for_while_dowhile/Aula_while.java
+++ Java/ProjetoAula3_8/src/for_while_dowhile/Aula_while.java
@@ -6,14 +6,20 @@ public class Aula_while {
 
 	public static void main(String[] args) {
 		char saida = 'N';
+		int numero = 0, somatorio = 0;
 		
 		Scanner entrada = new Scanner(System.in);
 		
 		while (saida != 'S') {
-			System.out.println("Gostaria de continuar? (S - Sim / N - Não ");
+			System.out.println("Digite um número: ");
+			numero = entrada.nextInt();
+			entrada.nextLine();
+			System.out.println("Gostaria de sair? (S - Sim / N - Não)");
 			saida = entrada.nextLine().charAt(0);
+			somatorio += numero;
+			System.out.printf("O somatório é igual a: %d \n", somatorio);
 		}
-
+		System.out.printf("O somatório é igual a: %d", somatorio);
 	}
 
 }
